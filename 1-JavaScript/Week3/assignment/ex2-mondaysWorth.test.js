bcc9a5e492c7bb6a0b8308723278ed41454baba1
hshamsadd <hshamsadd@gmail.com>
2025-06-04 17:10:54 +0200

    Update ex2-mondaysWorth.test.js

--- 1-JavaScript/Week3/assignment/ex2-mondaysWorth.test.js
+++ 1-JavaScript/Week3/assignment/ex2-mondaysWorth.test.js
@@ -30,24 +30,11 @@ const mondayTasks = [
 
 const hourlyRate = 25;
 
-// function computeEarnings(mondayTasks, hourlyRate) {
-//   // TODO complete this function
-//   let totalHours = mondayTasks
-//     .map((task) => task['duration'])
-//     .reduce((acc, currVal) => acc + currVal);
-//   totalHours /= 60;
-//   return `€${totalHours * hourlyRate.toFixed(2)}`;
-// }
-
 function computeEarnings(mondayTasks, hourlyRate) {
-  const totalMinutes = mondayTasks.reduce(
-    (acc, task) => acc + task.duration,
-    0
-  );
-  const totalHours = totalMinutes / 60;
-  const totalEarnings = totalHours * hourlyRate;
-
-  return `€${totalEarnings.toFixed(2)}`;
+const totalMinutes = mondayTasks.map(task => task.duration).reduce((sum, minutes) => sum + minutes, 0);
+const totalHours = totalMinutes / 60;
+const totalEarnings = totalHours * hourlyRate;
+return `€${totalEarnings.toFixed(2)}`;
 }
 
 // ! Unit tests (using Jest)
